2171bc3464113bff741229891e1a4f3fb9fad486
Hzhxi9 <hzhixi9@gmail.com>
2021-08-22 10:16:05 +0800

    render-slot

--- src/core/instance/render-helpers/render-slot.js
+++ src/core/instance/render-helpers/render-slot.js
@@ -1,40 +1,77 @@
 /* @flow */
 
-import { extend, warn, isObject } from 'core/util/index'
+import { extend, warn, isObject } from "core/util/index";
 
 /**
  * Runtime helper for rendering <slot>
+ * 用于呈现<slot>的运行时帮助程序
+ * @param {*} name 子组件中slot的name，匿名default
+ * @param {*} fallbackRender 子组件插槽中默认内容VNode， 如果没有插槽内容，则显示该内容
+ * @param {*} props 子组件传递到插槽的props
+ * @param {*} bindObject 针对 <slot v-bind="obj" /> obj必须是一个对象
+ * @returns
  */
-export function renderSlot (
+export function renderSlot(
   name: string,
   fallbackRender: ?((() => Array<VNode>) | Array<VNode>),
   props: ?Object,
   bindObject: ?Object
 ): ?Array<VNode> {
-  const scopedSlotFn = this.$scopedSlots[name]
-  let nodes
+  /**判断父组件是否传递作用域插槽 */
+  const scopedSlotFn = this.$scopedSlots[name];
+
+  /**虚拟DOM */
+  let nodes;
+
   if (scopedSlotFn) {
-    // scoped slot
-    props = props || {}
+    /**处理作用域插槽 */
+
+    props = props || {};
     if (bindObject) {
-      if (process.env.NODE_ENV !== 'production' && !isObject(bindObject)) {
-        warn('slot v-bind without argument expects an Object', this)
+      /**bindObject 必须是一个对象 */
+      if (process.env.NODE_ENV !== "production" && !isObject(bindObject)) {
+        warn("slot v-bind without argument expects an Object", this);
       }
-      props = extend(extend({}, bindObject), props)
+
+      /**合并对象和props属性 */
+      props = extend(extend({}, bindObject), props);
     }
+
+    /**传入props生成相应的VNode */
     nodes =
       scopedSlotFn(props) ||
-      (typeof fallbackRender === 'function' ? fallbackRender() : fallbackRender)
+      (typeof fallbackRender === "function"
+        ? fallbackRender()
+        : fallbackRender);
   } else {
+    /**处理不是作用域插槽 */
+
     nodes =
       this.$slots[name] ||
-      (typeof fallbackRender === 'function' ? fallbackRender() : fallbackRender)
+      (typeof fallbackRender === "function"
+        ? fallbackRender()
+        : fallbackRender);
   }
 
-  const target = props && props.slot
+  /**props属性存在并且属性的插槽存在props.slot */
+  const target = props && props.slot;
+
+
+  /**
+   * 如果还需要向子组件的子组件传递slot
+   * Bar组件： <p class="bar"><slot name="foo"></slot></p>
+   * Foo组件： <p class="foo"><bar><slot slot="foo"/></bar></p>
+   * Main组件： <p><foo>hello</foo></p>
+   * 
+   * 最终渲染： <p class="foo"><p class="bar">hello</p></p>
+   */
+  
   if (target) {
-    return this.$createElement('template', { slot: target }, nodes)
+    /**
+     * 创建模板，创建DOM节点，虚拟DOM需要渲染的数据结构
+     */
+    return this.$createElement("template", { slot: target }, nodes);
   } else {
-    return nodes
+    return nodes;
   }
 }
