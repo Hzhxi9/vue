0fbeec302b575a02b1526d657aed9be406ff283f
Hzhxi9 <hzhixi9@gmail.com>
2021-08-30 12:04:19 +0800

    event

--- src/platforms/web/runtime/modules/events.js
+++ src/platforms/web/runtime/modules/events.js
@@ -1,50 +1,72 @@
 /* @flow */
 
-import { isDef, isUndef } from 'shared/util'
-import { updateListeners } from 'core/vdom/helpers/index'
-import { isIE, isFF, supportsPassive, isUsingMicroTask } from 'core/util/index'
-import { RANGE_TOKEN, CHECKBOX_RADIO_TOKEN } from 'web/compiler/directives/model'
-import { currentFlushTimestamp } from 'core/observer/scheduler'
+import { isDef, isUndef } from "shared/util";
+import { updateListeners } from "core/vdom/helpers/index";
+import { isIE, isFF, supportsPassive, isUsingMicroTask } from "core/util/index";
+import {
+  RANGE_TOKEN,
+  CHECKBOX_RADIO_TOKEN,
+} from "web/compiler/directives/model";
+import { currentFlushTimestamp } from "core/observer/scheduler";
 
 // normalize v-model event tokens that can only be determined at runtime.
 // it's important to place the event as the first in the array because
 // the whole point is ensuring the v-model callback gets called before
 // user-attached handlers.
-function normalizeEvents (on) {
+function normalizeEvents(on) {
   /* istanbul ignore if */
   if (isDef(on[RANGE_TOKEN])) {
     // IE input[type=range] only supports `change` event
-    const event = isIE ? 'change' : 'input'
-    on[event] = [].concat(on[RANGE_TOKEN], on[event] || [])
-    delete on[RANGE_TOKEN]
+    const event = isIE ? "change" : "input";
+    on[event] = [].concat(on[RANGE_TOKEN], on[event] || []);
+    delete on[RANGE_TOKEN];
   }
   // This was originally intended to fix #4521 but no longer necessary
   // after 2.5. Keeping it for backwards compat with generated code from < 2.4
   /* istanbul ignore if */
   if (isDef(on[CHECKBOX_RADIO_TOKEN])) {
-    on.change = [].concat(on[CHECKBOX_RADIO_TOKEN], on.change || [])
-    delete on[CHECKBOX_RADIO_TOKEN]
+    on.change = [].concat(on[CHECKBOX_RADIO_TOKEN], on.change || []);
+    delete on[CHECKBOX_RADIO_TOKEN];
   }
 }
 
-let target: any
+let target: any;
 
-function createOnceHandler (event, handler, capture) {
-  const _target = target // save current target element in closure
-  return function onceHandler () {
-    const res = handler.apply(null, arguments)
+/**
+ * 柯里化函数，返回一个直接调用函数的方法，调用完就删除事件
+ * @param {*} event 转义过的事件
+ * @param {*} handler 事件名
+ * @param {*} capture 事件捕获或冒泡行为
+ * @returns
+ */
+function createOnceHandler(event, handler, capture) {
+  const _target = target; // save current target element in closure
+  return function onceHandler() {
+    const res = handler.apply(null, arguments);
     if (res !== null) {
-      remove(event, onceHandler, capture, _target)
+      remove(
+        event /**事件名 */,
+        onceHandler /**绑定的事件 */,
+        capture /**事件捕获或冒行为 */,
+        _target /**真实DOM */
+      );
     }
-  }
+  };
 }
 
 // #9446: Firefox <= 53 (in particular, ESR 52) has incorrect Event.timeStamp
 // implementation and does not fire microtasks in between event propagation, so
 // safe to exclude.
-const useMicrotaskFix = isUsingMicroTask && !(isFF && Number(isFF[1]) <= 53)
+const useMicrotaskFix = isUsingMicroTask && !(isFF && Number(isFF[1]) <= 53);
 
-function add (
+/**
+ * 为
+ * @param {*} name
+ * @param {*} handler
+ * @param {*} capture
+ * @param {*} passive
+ */
+function add(
   name: string,
   handler: Function,
   capture: boolean,
@@ -57,8 +79,8 @@ function add (
   // and the handler would only fire if the event passed to it was fired
   // AFTER it was attached.
   if (useMicrotaskFix) {
-    const attachedTimestamp = currentFlushTimestamp
-    const original = handler
+    const attachedTimestamp = currentFlushTimestamp;
+    const original = handler;
     handler = original._wrapper = function (e) {
       if (
         // no bubbling, should always fire.
@@ -76,20 +98,28 @@ function add (
         // starting reference
         e.target.ownerDocument !== document
       ) {
-        return original.apply(this, arguments)
+        return original.apply(this, arguments);
       }
-    }
+    };
   }
+  /**
+   * 为真实DOM添加事件
+   */
   target.addEventListener(
-    name,
-    handler,
-    supportsPassive
-      ? { capture, passive }
-      : capture
-  )
+    name /**事件名称 */,
+    handler /**转译过的事件, DOM 绑定的事件 */,
+    supportsPassive ? { capture, passive } : capture /**事件是捕获还是冒泡 */
+  );
 }
 
-function remove (
+/**
+ * 删除真实DOM的事件
+ * @param {*} name 事件名称
+ * @param {*} handler 转译过的事件, DOM绑定的事件
+ * @param {*} capture 事件捕获或者冒泡行为
+ * @param {*} _target 真实DOM
+ */
+function remove(
   name: string,
   handler: Function,
   capture: boolean,
@@ -99,22 +129,45 @@ function remove (
     name,
     handler._wrapper || handler,
     capture
-  )
+  );
 }
 
-function updateDOMListeners (oldVnode: VNodeWithData, vnode: VNodeWithData) {
+/**
+ * 更新DOM事件
+ * @param {*} oldVnode
+ * @param {*} vnode
+ * @returns
+ */
+function updateDOMListeners(oldVnode: VNodeWithData, vnode: VNodeWithData) {
+  /**
+   * 判断是否定义了事件on, 都没有定义直接结束
+   */
   if (isUndef(oldVnode.data.on) && isUndef(vnode.data.on)) {
-    return
+    return;
   }
-  const on = vnode.data.on || {}
-  const oldOn = oldVnode.data.on || {}
-  target = vnode.elm
-  normalizeEvents(on)
-  updateListeners(on, oldOn, add, remove, createOnceHandler, vnode.context)
-  target = undefined
+  const on = vnode.data.on || {};
+  const oldOn = oldVnode.data.on || {};
+
+  /**获取真实DOM */
+  target = vnode.elm;
+
+  normalizeEvents(on);
+
+  /**
+   * 更新数据源并且为新的值添加函数， 旧值删除函数
+   */
+  updateListeners(
+    on /**新的事件对象 */,
+    oldOn /**旧的事件对象 */,
+    add /**添加真实DOM的事件函数 */,
+    remove /**删除真实DOM的事件函数 */,
+    createOnceHandler,
+    vnode.context /**vue实例化对象(new Vue) or 组件(构造函数实例化的对象) */
+  );
+  target = undefined;
 }
 
 export default {
   create: updateDOMListeners,
-  update: updateDOMListeners
-}
+  update: updateDOMListeners,
+};
